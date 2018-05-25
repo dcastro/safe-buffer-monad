9393d95f3bdda77fe3bdddc2fee529137ef3daca
Diogo Castro <dc@diogocastro.com>
2018-05-25 17:28:57 +0100

    Added lifted STM functions to avoid using `liftIO` all the time

--- src/SafeBuffer.hs
+++ src/SafeBuffer.hs
@@ -3,13 +3,17 @@
 {-# LANGUAGE InstanceSigs               #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 
-module SafeBuffer where
+module SafeBuffer
+  ( SafeBufferMonad(..)
+  , SafeBufferConcurrentT
+  , runConcurrently
+  , tryRunConcurrently
+  ) where
 
 import           Control.Concurrent.STM
 import           Control.Monad.Catch
 import           Control.Monad.Reader
 import           Control.Monad.Writer
-import           Data.IORef
 
 class Monad m => SafeBufferMonad s m where
   writeBuffer :: s -> m ()
@@ -27,27 +31,41 @@ newtype SafeBufferConcurrentT s m a = SafeBufferConcurrentT { run :: ReaderT (TV
            , MonadMask
            )
 
-runConcurrent :: (MonadIO m, MonadMask m, Monoid s)
-              => (s -> m b)
-              -> SafeBufferConcurrentT s m a
-              -> m a
-runConcurrent finalize sb =
+runConcurrently :: (MonadIO m, MonadMask m, Monoid s)
+                => (s -> m b)
+                -> SafeBufferConcurrentT s m a
+                -> m a
+runConcurrently finalize sb =
   bracket
-    (liftIO $ newTVarIO mempty)
-    (\tv -> liftIO (readTVarIO tv) >>= finalize)
+    (newTVarIO' mempty)
+    (\tv -> readTVarIO' tv >>= finalize)
     (\tv -> runReaderT (run sb) tv)
 
 tryRunConcurrently :: (MonadIO m, MonadMask m, Monoid s, Exception e)
                    => SafeBufferConcurrentT s m a
                    -> m (s, Either e a)
 tryRunConcurrently sb = do
-  tv <- liftIO $ newTVarIO mempty
+  tv <- newTVarIO' mempty
   result <- try $ runReaderT (run sb) tv
-  buffer <- liftIO $ readTVarIO tv
+  buffer <- readTVarIO' tv
   pure (buffer, result)
   
 instance (Monad m, MonadIO m, Monoid s) => SafeBufferMonad s (SafeBufferConcurrentT s m) where
   writeBuffer :: s -> SafeBufferConcurrentT s m ()
   writeBuffer msg =
     SafeBufferConcurrentT $ ReaderT $ \tvar -> do
-      liftIO $ atomically $ modifyTVar tvar (`mappend` msg)
+      modifyTVarIO' tvar (`mappend` msg)
+
+--------------------------------------------------------------------------------
+-- Lifted STM functions
+--------------------------------------------------------------------------------
+newTVarIO' :: MonadIO m => a -> m (TVar a)
+newTVarIO' = liftIO . newTVarIO
+
+readTVarIO' :: MonadIO m => TVar a -> m a
+readTVarIO' = liftIO . readTVarIO
+
+modifyTVarIO' :: MonadIO m => TVar a -> (a -> a) -> m ()
+modifyTVarIO' tv = liftIO . atomically . modifyTVar tv
+
+
