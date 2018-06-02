9cf501b5dc9ea9fbc8b9dfc1a0b4c7fdfcf55d2b
Diogo Castro <dc@diogocastro.com>
2018-06-02 19:26:39 +0100

    Removed redundant language extensions

--- src/SafeBuffer.hs
+++ src/SafeBuffer.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE AllowAmbiguousTypes        #-}
-{-# LANGUAGE ExplicitForAll             #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE InstanceSigs               #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TypeApplications           #-}
 
@@ -38,89 +36,6 @@ class Monad m => SafeBufferMonad s m | m -> s where
   clearBuffer :: m s
   modifyBuffer :: (s -> s) -> m ()
 
---------------------------------------------------------------------------------
--- SafeBufferConcurrentT
---------------------------------------------------------------------------------
-newtype SafeBufferConcurrentT s m a =
-  SafeBufferConcurrentT { runBufferConcurrentT :: ReaderT (TVar s) m a }
-  deriving ( Functor
-           , Applicative
-           , Alternative
-           , Monad
-           , MonadTrans
-           , MonadIO
-           , MonadReader (TVar s)
-           , MonadWriter r
-           , MonadState s
-           , MonadZip
-           , MonadThrow
-           , MonadCatch
-           , MonadMask
-           , MonadError e
-           , MonadFail
-           , MonadPlus
-           , MonadCont
-           , MonadFix
-           )
-
--- | Recover from and swallow exceptions of type `e`
-execBufferConcurrently ::
-     forall e s m a
-   . (MonadIO m, MonadCatch m, Monoid s, Exception e)
-  => SafeBufferConcurrentT s m a
-  -> m s
-execBufferConcurrently sb = do
-  tvar <- newTVarIO mempty
-  catch @m @e
-    (runReaderT (runBufferConcurrentT sb) tvar >> readTVarIO tvar)
-    (\_exception -> readTVarIO tvar)
-
--- | Runs a buffer and applies a given function to it.
--- | If an exception occurs while running the buffer,
--- | the function still runs before the exception is rethrown.
-runBufferConcurrently ::
-     forall s m a b
-   . (MonadIO m, MonadMask m, Monoid s)
-  => (s -> m b)
-  -> SafeBufferConcurrentT s m a
-  -> m a
-runBufferConcurrently finalize sb =
-  bracket
-    (newTVarIO mempty)
-    (\tvar -> readTVarIO tvar >>= finalize)
-    (\tvar -> runReaderT (runBufferConcurrentT sb) tvar)
-
--- | Runs a buffer and returns it, along with either an exception or the computation's result.
-tryRunBufferConcurrently ::
-     forall e s m a
-   . (MonadIO m, MonadCatch m, Monoid s, Exception e)
-  => SafeBufferConcurrentT s m a
-  -> m (s, Either e a)
-tryRunBufferConcurrently sb = do
-  tvar <- newTVarIO mempty
-  result <- try $ runReaderT (runBufferConcurrentT sb) tvar
-  buffer <- readTVarIO tvar
-  pure (buffer, result)
-
-instance (MonadIO m, Monoid s) => SafeBufferMonad s (SafeBufferConcurrentT s m) where
-  readBuffer :: SafeBufferConcurrentT s m s
-  readBuffer =
-    SafeBufferConcurrentT $ ReaderT $ \tvar ->
-      readTVarIO tvar
-      
-  writeBuffer :: s -> SafeBufferConcurrentT s m ()
-  writeBuffer msg = modifyBuffer (`mappend` msg)
-
-  clearBuffer :: SafeBufferConcurrentT s m s
-  clearBuffer = 
-    SafeBufferConcurrentT $ ReaderT $ \tvar ->
-      atomically $ swapTVar tvar mempty
-
-  modifyBuffer :: (s -> s) -> SafeBufferConcurrentT s m ()
-  modifyBuffer f =
-    SafeBufferConcurrentT $ ReaderT $ \tvar ->
-      atomically $ modifyTVar' tvar f
-
 
 --------------------------------------------------------------------------------
 -- SafeBufferT
@@ -146,6 +61,27 @@ newtype SafeBufferT s m a =
            , MonadCont
            , MonadFix
            )
+
+instance (MonadIO m, Monoid s) => SafeBufferMonad s (SafeBufferT s m) where
+  readBuffer :: SafeBufferT s m s
+  readBuffer =
+    SafeBufferT $ ReaderT $ \ref ->
+      readIORef ref
+
+  writeBuffer :: s -> SafeBufferT s m ()
+  writeBuffer msg = modifyBuffer (`mappend` msg)
+
+  clearBuffer :: SafeBufferT s m s
+  clearBuffer = 
+    SafeBufferT $ ReaderT $ \ref -> do
+      buffer <- readIORef ref
+      writeIORef ref mempty
+      pure buffer
+
+  modifyBuffer :: (s -> s) -> SafeBufferT s m ()
+  modifyBuffer f = 
+    SafeBufferT $ ReaderT $ \ref ->
+      modifyIORef' ref f
 
 -- | Recover from and swallow exceptions of type `e`
 execBuffer ::
@@ -186,23 +122,85 @@ tryRunBuffer sb = do
   buffer <- readIORef ref
   pure (buffer, result)
 
-instance (MonadIO m, Monoid s) => SafeBufferMonad s (SafeBufferT s m) where
-  readBuffer :: SafeBufferT s m s
-  readBuffer =
-    SafeBufferT $ ReaderT $ \ref ->
-      readIORef ref
+--------------------------------------------------------------------------------
+-- SafeBufferConcurrentT
+--------------------------------------------------------------------------------
+newtype SafeBufferConcurrentT s m a =
+  SafeBufferConcurrentT { runBufferConcurrentT :: ReaderT (TVar s) m a }
+  deriving ( Functor
+           , Applicative
+           , Alternative
+           , Monad
+           , MonadTrans
+           , MonadIO
+           , MonadReader (TVar s)
+           , MonadWriter r
+           , MonadState s
+           , MonadZip
+           , MonadThrow
+           , MonadCatch
+           , MonadMask
+           , MonadError e
+           , MonadFail
+           , MonadPlus
+           , MonadCont
+           , MonadFix
+           )
 
-  writeBuffer :: s -> SafeBufferT s m ()
+instance (MonadIO m, Monoid s) => SafeBufferMonad s (SafeBufferConcurrentT s m) where
+  readBuffer :: SafeBufferConcurrentT s m s
+  readBuffer =
+    SafeBufferConcurrentT $ ReaderT $ \tvar ->
+      readTVarIO tvar
+
+  writeBuffer :: s -> SafeBufferConcurrentT s m ()
   writeBuffer msg = modifyBuffer (`mappend` msg)
 
-  clearBuffer :: SafeBufferT s m s
+  clearBuffer :: SafeBufferConcurrentT s m s
   clearBuffer = 
-    SafeBufferT $ ReaderT $ \ref -> do
-      buffer <- readIORef ref
-      writeIORef ref mempty
-      pure buffer
+    SafeBufferConcurrentT $ ReaderT $ \tvar ->
+      atomically $ swapTVar tvar mempty
 
-  modifyBuffer :: (s -> s) -> SafeBufferT s m ()
-  modifyBuffer f = 
-    SafeBufferT $ ReaderT $ \ref ->
-      modifyIORef' ref f
+  modifyBuffer :: (s -> s) -> SafeBufferConcurrentT s m ()
+  modifyBuffer f =
+    SafeBufferConcurrentT $ ReaderT $ \tvar ->
+      atomically $ modifyTVar' tvar f
+
+-- | Recover from and swallow exceptions of type `e`
+execBufferConcurrently ::
+     forall e s m a
+   . (MonadIO m, MonadCatch m, Monoid s, Exception e)
+  => SafeBufferConcurrentT s m a
+  -> m s
+execBufferConcurrently sb = do
+  tvar <- newTVarIO mempty
+  catch @m @e
+    (runReaderT (runBufferConcurrentT sb) tvar >> readTVarIO tvar)
+    (\_exception -> readTVarIO tvar)
+
+-- | Runs a buffer and applies a given function to it.
+-- | If an exception occurs while running the buffer,
+-- | the function still runs before the exception is rethrown.
+runBufferConcurrently ::
+     forall s m a b
+   . (MonadIO m, MonadMask m, Monoid s)
+  => (s -> m b)
+  -> SafeBufferConcurrentT s m a
+  -> m a
+runBufferConcurrently finalize sb =
+  bracket
+    (newTVarIO mempty)
+    (\tvar -> readTVarIO tvar >>= finalize)
+    (\tvar -> runReaderT (runBufferConcurrentT sb) tvar)
+
+-- | Runs a buffer and returns it, along with either an exception or the computation's result.
+tryRunBufferConcurrently ::
+     forall e s m a
+   . (MonadIO m, MonadCatch m, Monoid s, Exception e)
+  => SafeBufferConcurrentT s m a
+  -> m (s, Either e a)
+tryRunBufferConcurrently sb = do
+  tvar <- newTVarIO mempty
+  result <- try $ runReaderT (runBufferConcurrentT sb) tvar
+  buffer <- readTVarIO tvar
+  pure (buffer, result)
